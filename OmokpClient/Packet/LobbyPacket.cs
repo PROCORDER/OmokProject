2449b834112fe325c1b9a2f703546db8c74fa4e5
junwon <apro500@naver.com>
2023-07-30 17:00:05 +0900

    add form directory and make robbyPacket in robby player actions make a room,chat, check player's friend list and in room, host can something, inviting friend, kick player,Boom Room and chatting with player

---
+++ OmokpClient/Packet/LobbyPacket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmokClient
+{
+    public enum ELobbyaction
+    {
+        MakeRoom=0,
+        Playwithfriend,
+        Message,
+        Exit
+
+    }
+    public enum EWaitingRoomHostAction
+    {
+        Play=0,
+        Kick,
+        Invite,
+        Message,
+        Boom
+
+
+    }
+    public enum EWaitingRoomGuestAction
+    {
+        Message=0,
+        Exit
+    }
+
+    class LobbyPacket
+    {
+        string lobbyMyName;
+        string message;
+        int lobbyAction;
+
+        bool inRoom;
+        string roomName;
+        
+        bool imHost;
+        int hostAction;
+
+        int guestAction;     
+    }
+}
