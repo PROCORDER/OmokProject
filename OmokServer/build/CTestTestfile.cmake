58434d0052d37af2f3eb77f07600d119edb758e0
junwon <apro500@naver.com>
2023-08-07 17:09:11 +0900

    change serierlize method basic-> Protobuf and made OmokServer directory. i will make c++ server using CMake.

---
+++ OmokServer/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Desktop/study/OMOKproject/OmokServer
+# Build directory: C:/Users/user/Desktop/study/OMOKproject/OmokServer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
